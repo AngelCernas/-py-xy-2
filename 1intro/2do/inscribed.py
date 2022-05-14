9cbb9cb51fec43f937b6fd3add0bd9ee175a2663
AngelCernas <56774493+AngelCernas@users.noreply.github.com>
2022-05-14 15:42:52 -0500

    Update inscribed.py

--- 1intro/2do/inscribed.py
+++ 1intro/2do/inscribed.py
@@ -4,3 +4,37 @@
 #
 # zeeAlso
 # https://keisan.casio.com/exec/system/1223428152
+
+import math as m
+a=None
+b=None
+c=None
+
+while True:
+    try:
+        a=float(input('Esribe el primer lado del triangulo'))
+        break
+    except:
+        print('Debes escribir un numero.')
+
+while True:
+    try:
+        b=float(input('Esribe el segundo lado del triangulo'))
+        break
+    except:
+        print('Debes escribir un numero.')
+
+while True:
+    try:
+        c=float(input('Esribe el tercer lado del triangulo'))
+        break
+    except:
+        print('Debes escribir un numero.')
+
+
+s = (a+b+c)*(1/2)
+
+print(s)
+
+r = m.sqrt(s*((s-a)*(s-b)*(s-c))/s)
+print(r)
