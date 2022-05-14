9b5df264b60e930512fd81c8b147ba3641d80db6
AngelCernas <56774493+AngelCernas@users.noreply.github.com>
2022-05-14 15:45:29 -0500

    Update stats.py

--- 2fundamentals/2do/stats.py
+++ 2fundamentals/2do/stats.py
@@ -16,3 +16,23 @@ def range(l): pass
 def std(l): pass
 
 def mode(l): pass
+
+
+from cmath import sin
+import math
+
+
+print("\n\n  funciones y=sin(2x) para x=1.3\n Sobre la función y=e^(1-2x) en x=-0.45 \n")
+
+print("\n\nFunción y=sin(2x) para x=1.3")
+
+y = sin(2*1.3)
+print("Resultado 1:")
+print(y)
+
+
+print("\n\nFunción y=e^(1-2x) en x=-0.45 ")
+
+y2 = math.exp(1-2*(-0.45))
+print("= 2:")
+print(y2)
