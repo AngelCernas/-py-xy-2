949f41955950826a3ee3cb0991e3eeda31cf06c0
AngelCernas <56774493+AngelCernas@users.noreply.github.com>
2022-05-25 15:38:43 -0500

    Update 2do2.py

--- 4roots/2do/2do2.py
+++ 4roots/2do/2do2.py
@@ -1,16 +1,4 @@
-#
-# generate a function to produce points 
-# to be used as x-axis
-# 
-# INPUT
-# -> a initial point
-# -> b final point
-# -> c increment
-#
-# OUTPUT
-# <- list of points
-#
-# for instance for this range [1,10,.1]
-# it will produce 100 points
-# [1.0, 1.1, ... , 9.9, 10]
-#
+import numpy as npy
+
+count = npy.arange(0, 10, 0.1)
+print(count)
